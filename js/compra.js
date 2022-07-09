6c7e74389bfa11d6abe7f8ba61e65dc26270a4f9
nicoarias47 <ariasn1995@gmail.com>
2022-07-09 16:45:46 -0300

    pagina de compra js finalizado

--- js/compra.js
+++ js/compra.js
@@ -1,7 +1,34 @@
-const btnCompra = document.querySelector("#btn-comprar");
+const deleteStorage = () => {
+  localStorage.clear();
+};
 
-btnCompra.addEventListener("submit", () => {
-  if (btnCompra.willValidate) {
-    window.location.href = "../pages/armatupc.html";
-  }
+const btnCompra = document.querySelector("#btn-comprar");
+const form = document.querySelector("#formulario");
+
+const DateTime = luxon.DateTime;
+
+const now = DateTime.now();
+const llegada = DateTime.fromObject({ day: now.day + 10 });
+
+form.addEventListener("submit", (e) => {
+  const name = document.querySelector("#validationDefault01").value;
+  const lastName = document.querySelector("#validationDefault02").value;
+  e.preventDefault();
+  e.stopPropagation();
+  Swal.fire({
+    title: `Gracias por confiar en HYPE! ${name} ${lastName}`,
+    html: `<span class="sweet-fechas">Fecha actual: ${now.toLocaleString()}</span>
+          <span class="sweet-fechas">Tu compra llegara aprox: ${llegada.toLocaleString()}</span>`,
+    imageUrl: "https://unsplash.it/400/200",
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: "Gracias",
+    confirmButtonText: "Continuar",
+    confirmButtonColor: "#03cc90",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.location.href = "../index.html";
+    }
+  });
+  deleteStorage();
 });
