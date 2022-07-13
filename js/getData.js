6d6c3bfec1f36f3f068bfb83a04516faf7b0a153
nicoarias47 <ariasn1995@gmail.com>
2022-07-12 21:33:21 -0300

    index carousel

--- js/getData.js
+++ js/getData.js
@@ -1,7 +1,16 @@
-const API = "../products.json";
+const API = "../data/products.json";
 
 export const pedirProduct = async () => {
   const resp = await fetch(API);
+  const data = await resp.json();
+
+  return data;
+};
+
+const IMG = "../data/img.json";
+
+export const pedirImg = async () => {
+  const resp = await fetch(IMG);
   const data = await resp.json();
 
   return data;
