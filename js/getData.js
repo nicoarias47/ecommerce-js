aa4e4d5e0ff9df1c7c1a2c0b6c5b9380bd2b91bc
nicoarias47 <ariasn1995@gmail.com>
2022-07-13 17:18:07 -0300

    fixed 5

--- js/getData.js
+++ js/getData.js
@@ -7,7 +7,7 @@ export const pedirProduct = async () => {
   return data;
 };
 
-const IMG = "../data/url.json";
+const IMG = "../images.json";
 
 export const pedirImg = async () => {
   const resp = await fetch(IMG);
